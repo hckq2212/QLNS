7ba8c7251dd9c30f43e59bf185335099a6a94271
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2025-11-26 11:24:06 +0700

    Test deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,24 @@
+// import { Pool } from 'pg';
+
+// const db = new Pool ({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+
+// })
+
+// export default db;
+
+
 import { Pool } from 'pg';
 
-const db = new Pool ({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-
-})
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default db;
